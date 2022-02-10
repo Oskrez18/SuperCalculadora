c08b9914e8b1eaf0567e66cb59d94f58f26deb1c
Jesús Jurado Barrau <jjurbar021@g.educaand.es>
2022-02-10 09:10:43 +0100

    Creacion de clases ecuacion 1 y 2

---
+++ src/clases/Ecuacion2.java
@@ -0,0 +1,45 @@
+public class Ecuacion2 {
+
+    private int num01;
+    private int num02;
+
+    public Constructor() {
+
+    }
+
+    public int getNum01() {
+        return num01;
+    }
+
+    public void setNum01(int num01) {
+        this.num01 = num01;
+    }
+
+    public int getNum02() {
+        return num02;
+    }
+
+    public void setNum02(int num02) {
+        this.num02 = num02;
+    }
+
+    public int[] calcular(int num01, int num02, int num03) {
+        int[] result = new int[2];
+
+        if (Math.sqrt(Math.pow(num02, 2)) > 0) {
+            result[0] = ((-num02) + (Math.sqrt(Math.pow(num02, 2))) - (4*num01*num03)) / 2 * num01;
+            result[1] = ((-num02) - (Math.sqrt(Math.pow(num02, 2))) - (4*num01*num03)) / 2 * num01;
+        } else if (num02 == 0 && num03 == 0) {
+            result[0] = 0;
+            result[1] = 1;
+        }  else if (num03 == 0) {
+            result[0] = 0;
+            result[1] = -num02/num01;
+        } else if (num02 == 0) {
+            result[0] = (Math.sqrt(-num03 / num01));
+            result[0] = -(Math.sqrt(-num03 / num01));
+        }
+        
+        return result;
+    }
+}
